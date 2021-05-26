f73f13e45b0b27d04a8da3a60315437ea99032b3
Charles Burt <ctf_exe@hotmail.com>
2021-05-26 18:09:37 +0100

    Adding files

---
+++ Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<School> Schools { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SchoolsData");
+        }
+    }
+}
---
+++ View/Program.cs
@@ -0,0 +1,28 @@
+﻿using Data;
+using Model;
+using System;
+using System.Linq;
+
+namespace View
+{
+    class Program
+    {
+        public static Context context = new Context();
+        static void Main(string[] args)
+        {
+            context.Database.EnsureCreated();
+            Console.WriteLine("Enter School's name to add to database!");
+            var school = new School();
+            school.Name = Console.ReadLine();
+            context.Schools.Add(school);
+            context.SaveChanges();
+            var schoolList = context.Schools.ToList();
+            Console.WriteLine("Printing Schools");
+            foreach (var item in schoolList)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+        }
+    }
+}
